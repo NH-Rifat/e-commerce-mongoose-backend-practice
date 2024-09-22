4eeef4178b7e4a32ca17ae9e0bbf550ce0115a01
Hasan Rifat <rifatr729@gmail.com>
2024-09-22 18:05:28 +0600

    project initial setup configured

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+// import { studentRoutes } from './app/modules/student/student.route';
+const app: Application = express();
+const port = 3000;
+
+//parser
+app.use(express.json());
+app.use(cors());
+
+//application routes
+// app.use('/api/v1/students', studentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  res.send("Hello World");
+};
+
+app.get("/", getAController);
+
+export default app;
